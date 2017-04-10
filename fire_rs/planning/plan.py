be0eb4152ae65f30c52d789cb2f29fdecdb0874d
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-04-10 17:03:01 +0200

    [planning] Add a simple plan structure composed of a UAV model and sequence of waypoints.
    The class mainly provides structures to compute the duration of the plan based on the UAV model,
    as well as methods to build derived plan (e.g. with an additional waypoint) whose durations are
    incrementally computed.

---
+++ fire_rs/planning/plan.py
@@ -0,0 +1,93 @@
+
+
+from fire_rs.planning.uav import UAV
+
+
+class Plan:
+    """Immutable data structure that records a plan for a given UAV as a sequence of waypoints"""
+
+    def __init__(self, uav: 'UAV', waypoints, duration=None):
+        self.uav = uav
+        self.waypoints = list(waypoints)  # make a copy to avoid any mutation of the plan
+        self.duration = duration
+
+        # duration not provided, compute it based on the UAV model
+        if self.duration is None:
+            self.duration = self.uav.trajectory_length(self.waypoints)
+
+    @property
+    def length(self):
+        return len(self.waypoints)
+
+    def __repr__(self):
+        return "duration: {} -- length: {} -- {}".format(round(self.duration, 2), self.length, self.waypoints)
+
+    def __getitem__(self, item):
+        return self.waypoints[item]
+
+    def extend(self, waypoint, position_in_plan: int) -> 'Plan':
+        """Builds a new Plan by inserting a new waypoint at the given position in the plan.
+         The current plan is not modified and a new one is returned.
+         """
+        assert 0 <= position_in_plan <= len(self.waypoints)
+
+        # incrementally compute new duration
+        new_duration = self.duration
+        if len(self.waypoints) == 0:
+            pass  # only one point in the resulting trajectory, duration is still 0
+        elif position_in_plan == 0:
+            new_duration += self.uav.travel_time(waypoint, self.waypoints[0])
+        elif position_in_plan == len(self.waypoints):
+            new_duration += self.uav.travel_time(self.waypoints[-1], waypoint)
+        else:
+            new_duration -= self.uav.travel_time(self.waypoints[position_in_plan - 1], self.waypoints[position_in_plan])
+            new_duration += self.uav.travel_time(self.waypoints[position_in_plan - 1], waypoint)
+            new_duration += self.uav.travel_time(waypoint, self.waypoints[position_in_plan])
+        new_points = list(self.waypoints)
+        new_points.insert(position_in_plan, waypoint)
+
+        return Plan(self.uav, new_points, new_duration)
+
+    def remove(self, position_in_plan) -> 'Plan':
+        """Builds a new Plan by removing the waypoint at the given position in the plan.
+        The current plan is not modified and a new one is returned.
+        """
+        assert 0 <= position_in_plan < len(self.waypoints)
+
+        # incrementally compute new duration
+        new_duration = self.duration
+        if len(self.waypoints) == 1:
+            pass  # only one point in the resulting trajectory, duration is still 0
+        if position_in_plan == 0:
+            new_duration -= self.uav.travel_time(self.waypoints[0], self.waypoints[1])
+        elif position_in_plan == len(self.waypoints):
+            new_duration -= self.uav.travel_time(self.waypoints[-2], self.waypoints[-1])
+        else:
+            new_duration -= self.uav.travel_time(self.waypoints[position_in_plan - 1],
+                                                 self.waypoints[position_in_plan])
+            new_duration -= self.uav.travel_time(self.waypoints[position_in_plan],
+                                                 self.waypoints[position_in_plan + 1])
+            new_duration += self.uav.travel_time(self.waypoints[position_in_plan - 1],
+                                                 self.waypoints[position_in_plan + 1])
+
+        new_points = list(self.waypoints)
+        del new_points[position_in_plan]
+
+        return Plan(self.uav, new_points, new_duration)
+
+    def plot(self, blocking=False):
+        from matplotlib import patches
+        from matplotlib.path import Path
+        import matplotlib.pyplot as plt
+        xs = [x for (x, y) in self.waypoints]
+        ys = [y for (x, y) in self.waypoints]
+
+        path = Path(self.waypoints)
+        patch = patches.PathPatch(path, facecolor='none', lw=2)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.add_patch(patch)
+        ax.set_xlim(min(xs), max(xs))
+        ax.set_ylim(min(ys), max(ys))
+        plt.show(block=blocking)
+        return ax
