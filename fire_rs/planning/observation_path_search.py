f78bb0c45452bbbe8fe64ac19d6ce5902ae3fcd1
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2017-05-12 10:38:26 +0200

    Apply successive regressions on observation clusters until all of them are covered by an straight trajectory

--- fire_rs/planning/observation_path_search.py
+++ fire_rs/planning/observation_path_search.py
@@ -17,58 +17,17 @@ import logging
 from typing import Sequence
 
 
-camera_fov = (10, 10) # Horizontal, Vertical
-
-# Generate sample data
-centers = [[1, 1], [-1, -1], [1, -1], [2, 2]]
-points_of_interest, labels_true = make_blobs(n_samples=200, cluster_std=1, centers=20,
-                            random_state=21172)
-points_of_interest = points_of_interest * 10
-
-
-def density_weight(all_nodes):
-    '''Calculate a relevance weight of each node.
-
-    For every node, its weight is the sum of the inverse of the squared eculidean distance to any other node.
-    '''
-    # TODO: the distance to the UAV should be taken in account, so the closest cumulus of points
-    # get more importance than those that are further.
-    distance_matrix = scipy.spatial.distance.cdist(all_nodes, all_nodes, 'euclidean')
-    for i in range(len(distance_matrix)):
-        distance_matrix[i, i] = np.inf
-
-    weights = np.sum(1 / distance_matrix ** 2, axis=0)
-    max_weight = np.max(weights)
-    return weights / max_weight
-
-
-def tesselate():
-    poi_weights = density_weight(points_of_interest)
-
-    tesselation = scipy.spatial.Delaunay(points_of_interest)
-
-    fig = plt.figure()
-
-    plt.triplot(points_of_interest[:, 0], points_of_interest[:, 1], tesselation.simplices.copy())
-    plt.scatter(points_of_interest[:, 0], points_of_interest[:, 1], poi_weights * 100, c='red')
-
-    plt.show()
-
-
-#######################
-
-
 class VectorLine(namedtuple('VectorLine', ('point', 'vector'))):
     __slots__ = ()
 
     def to_point_slope_form(self):
-        return PointSlopeLine(self.point, self.vector[1]/self.vector[0])
+        return PointSlopeLine(self.point, self.vector[1] / self.vector[0])
 
     def evaluate(self, mu):
         return self.point + mu * self.vector
 
 
-class PointSlopeLine(namedtuple('PointSlopeLine',('point', 'slope'))):
+class PointSlopeLine(namedtuple('PointSlopeLine', ('point', 'slope'))):
     __slots__ = ()
 
     def to_vector_form(self):
@@ -122,9 +81,9 @@ def cluster(point_cloud, epsilon=10, min_samples=2, metric='euclidean', plot_clu
     unclustered = []
     for c in unique_labels:
         if c != -1:
-            clustered.append(point_cloud[labels==c])
+            clustered.append(point_cloud[labels == c])
         else:
-            unclustered = point_cloud[labels==c]
+            unclustered = point_cloud[labels == c]
 
     return clustered, unclustered
 
@@ -133,7 +92,7 @@ def intersection_of_lines(*lines: 'Sequence[VectorLine]'):
     assert len(lines) == 2
 
     # solving linear equation system using cramer's rule
-    M = np.array([[lines[0].vector[0], -lines[1].vector[0]],[lines[0].vector[1],-lines[1].vector[1]]])
+    M = np.array([[lines[0].vector[0], -lines[1].vector[0]], [lines[0].vector[1], -lines[1].vector[1]]])
     C = lines[1].point - lines[0].point
 
     x = np.linalg.det(np.column_stack([C, M[..., 1]])) / np.linalg.det(M)
@@ -152,7 +111,6 @@ def distance_point_to_line(point, line: 'VectorLine'):
 
 
 class SegmentObservationPath():
-
     def __init__(self, points, camera_fov, regressor=LinearRegression()):
         self.points = points
         self.camera_fov = camera_fov
@@ -163,11 +121,15 @@ class SegmentObservationPath():
         self.regression = None
         self.segment = None
 
-        self._linear_regression()
-        self._determine_coverage()
+        self._regression()
+        self._observe()
         self._determine_segment()
 
-    def _linear_regression(self):
+    @property
+    def coverage(self):
+        return np.count_nonzero(self.observed) / self.points.shape[0]
+
+    def _regression(self):
         """Get the line that fits the best a set of points using linear regression"""
         self.regressor.fit(X=self.points[..., 0][..., np.newaxis], y=self.points[..., 1])
         if isinstance(self.regressor, RANSACRegressor):
@@ -182,7 +144,7 @@ class SegmentObservationPath():
             self.line = PointSlopeLine(np.array([0, self.regressor.intercept_]),
                                        float(self.regressor.coef_)).to_vector_form()
 
-    def _determine_coverage(self):
+    def _observe(self):
         """Evaluate how many points are observed flying over a linear trajectory"""
         for i, point in enumerate(self.points):
             if distance_point_to_line(point, self.line) < self.camera_fov[0] / 2:
@@ -190,47 +152,49 @@ class SegmentObservationPath():
             else:
                 self.observed[i] = False
 
-        logging.info("{}: {}/{} points observed".format(str(type(self.regressor)), np.count_nonzero(self.observed), self.points.shape[0]))
+        logging.info("{}: {}/{} points observed".format(str(type(self.regressor)), np.count_nonzero(self.observed),
+                                                        self.points.shape[0]))
 
     def _determine_segment(self):
         line_pslope = self.line.to_point_slope_form()
 
-        most_left_point = min(self.points, key=lambda p: p[0])
+        most_left_point = min(self.points[self.observed], key=lambda p: p[0])
         p0 = np.array([most_left_point[0], line_pslope.evaluate(most_left_point[0])])
 
-        most_right_point = max(self.points, key=lambda p: p[0])
+        most_right_point = max(self.points[self.observed], key=lambda p: p[0])
         p1 = np.array([most_right_point[0], line_pslope.evaluate(most_right_point[0])])
 
         self.segment = np.array([p0, p1])
 
 
-def linear_paths():
-    clusters, unclustered = cluster(points_of_interest)
-    n_observations_cumulated = {'LinearRegression': 0, 'RANSACRegressor':0}
+def process_points(points, camera_fov, eps=10):
+    clusters, unclustered = cluster(points, epsilon=eps)
+    n_observations_cumulated = {'LinearRegression': 0, 'RANSACRegressor': 0}
     for c in clusters:
-        obs_path = SegmentObservationPath(c, camera_fov, regressor=LinearRegression())
-        observed = c[obs_path.observed]
-        plt.plot(observed[:, 0], observed[:, 1], 'o', markerfacecolor='green',
-                 markeredgecolor='green', markersize=10)
-        plt.plot([obs_path.segment[0][0], obs_path.segment[1][0]], [obs_path.segment[0][1], obs_path.segment[1][1]], linewidth=3, color='k')
-        n_observations_cumulated['LinearRegression'] += np.count_nonzero(obs_path.observed)
+        obs_paths = []
+        to_observe = c.copy()
+        while to_observe.shape[0] > max(1, int(0.01 * c.shape[0])):
+            path = SegmentObservationPath(to_observe, camera_fov, regressor=RANSACRegressor())
+            obs_paths.append(path)
+            to_observe = to_observe[~path.observed]
 
-        obs_path = SegmentObservationPath(c, camera_fov, regressor=RANSACRegressor())
-        observed = c[obs_path.observed]
-        plt.plot(observed[:, 0], observed[:, 1], 'o', markerfacecolor='blue',
-                 markeredgecolor='blue', markersize=5)
-        plt.plot([obs_path.segment[0][0], obs_path.segment[1][0]], [obs_path.segment[0][1], obs_path.segment[1][1]],
-                 linewidth=3, color='b')
-        n_observations_cumulated['RANSACRegressor'] += np.count_nonzero(obs_path.observed)
-        # pl1 = obs_path._left_perp_line.point + obs_path._left_perp_line.vector*-50
-        # pl2 = obs_path._left_perp_line.point + obs_path._left_perp_line.vector*50
-        # plt.plot([pl1[0], pl2[0]], [pl1[1], pl2[1]])
+            plt.plot(path.points[path.observed][:, 0], path.points[path.observed][:, 1], 'o', markerfacecolor='blue',
+                     markeredgecolor='blue', markersize=5)
+            plt.plot([path.segment[0][0], path.segment[1][0]],
+                     [path.segment[0][1], path.segment[1][1]],
+                     linewidth=3, color='blue')
+            plt.show()
 
     print(n_observations_cumulated)
 
-if __name__ == '__main__':
-    # tesselate()
-    logging.basicConfig(level=logging.DEBUG)
-    linear_paths()
-    print("stop")
 
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    camera_fov = (10, 10)  # Horizontal, Vertical
+
+    points_of_interest, labels_true = make_blobs(n_samples=200, cluster_std=1, centers=20,
+                                                 random_state=21)
+    points_of_interest = points_of_interest * 10
+
+    process_points(points_of_interest, camera_fov)
+    print("stop")
