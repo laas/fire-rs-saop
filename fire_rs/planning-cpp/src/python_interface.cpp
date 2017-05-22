f6f8d4bfd93e8f42e353b6eceaad845fa92e7d93
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-05-22 17:19:34 +0200

    [setup] Update setup.py to build the python extension from C++ planning code.

---
+++ fire_rs/planning-cpp/src/python_interface.cpp
@@ -0,0 +1,34 @@
+#include <pybind11/pybind11.h>
+#include <sstream>
+#include "trajectory.h"
+
+namespace py = pybind11;
+
+PYBIND11_PLUGIN(uav_planning) {
+    py::module m("uav_planning", "Generating primes in c++ with python bindings using pybind11");
+
+    py::class_<Waypoint>(m, "Waypoint")
+            .def(py::init<const double, const double, const double>())
+            .def_readonly("x", &Waypoint::x)
+            .def_readonly("y", &Waypoint::dir)
+            .def_readonly("dir", &Waypoint::dir)
+            .def("__repr__", &Waypoint::to_string);
+
+    py::class_<Segment>(m, "Segment")
+            .def(py::init<const Waypoint, const double>())
+            .def_readonly("start", &Segment::start)
+            .def_readonly("end", &Segment::end)
+            .def_readonly("length", &Segment::length);
+
+    py::class_<UAV>(m, "UAV")
+            .def(py::init<const double, const double>())
+            .def_readonly("rho", &UAV::rho)
+            .def_readonly("speed", &UAV::speed)
+            .def("travel_time", &UAV::travel_time, py::arg("origin"), py::arg("destination"));
+
+
+
+
+
+    return m.ptr();
+}
--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import find_packages, setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import pybind11
+
 setup(
     name="Fire RS",
     version="0.1",
@@ -10,12 +12,19 @@ setup(
     url="http://fire-rs.com",
     packages=find_packages(),
     license="GPLv3+",
-    install_requires=['affine', 'GDAL', 'numpy', 'pandas', 'pytz', 'matplotlib', 'sklearn'],
+    install_requires=['affine', 'GDAL', 'numpy', 'pandas', 'pytz', 'matplotlib', 'sklearn', 'pybind11'],
     ext_modules=[
         Extension("fire_rs.firemodel.rothermel", sources=["fire_rs/firemodel/rothermel.pyx"]),
         Extension("fire_rs.firemodel.environment", sources=["fire_rs/firemodel/environment.pyx"]),
         Extension("fire_rs.firemodel.fireshapes", sources=["fire_rs/firemodel/fireshapes.pyx"]),
         # Extension("fire_rs.firemodel.propagation", sources=["fire_rs/firemodel/propagation.pyx"]),
+        Extension(
+            'fire_rs.uav_planning',
+            ['fire_rs/planning-cpp/src/dubins.cpp', 'fire_rs/planning-cpp/src/python_interface.cpp'],
+            include_dirs=[pybind11.get_include(False), pybind11.get_include(True)],  # Path to pybind11 headers
+            extra_compile_args=["-std=c++11"],
+            language='c++'
+        ),
     ],
     cmdclass={'build_ext': build_ext}
 )
