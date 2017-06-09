7c6fba1a5b722cac152f702248ec848c46167b61
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-06-09 17:19:51 +0200

    [docker] Additional commands to automate build/tests

--- docker/run.sh
+++ docker/run.sh
@@ -10,27 +10,45 @@ ROOT_DIR="$(realpath $(dirname ${DOCKER_DIR}))"
 
 CONTAINER_NAME='saop'
 
+CONTAINER_START_CMD="docker run -it --cap-add=SYS_PTRACE -v ${ROOT_DIR}:/home/saop/code -v ${FIRERS_DATA}:/home/saop/data -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix ${CONTAINER_NAME}"
+
+BUILD_CMD='python3 setup.py build_ext --inplace'
+AUTO_BUILD_CMD="when-changed fire_rs/planning-cpp/src/* -c ${BUILD_CMD}"
+TEST_CMD="python3 -m unittest fire_rs.planning.test_uav_planning_cpp"
+
 case $1 in
+    'make_container')
+        echo "Building container $CONTAINER_NAME from directory $DOCKER_DIR"
+        echo ""
+        docker build -t ${CONTAINER_NAME} ${DOCKER_DIR}
+        ;;
     'build')
-    echo "Building container $CONTAINER_NAME from directory $DOCKER_DIR"
-    echo ""
-    docker build -t ${CONTAINER_NAME} ${DOCKER_DIR}
-    ;;
+        echo 'Building the project once (use autobuild for repeated)'
+        cmd="${CONTAINER_START_CMD} ${BUILD_CMD}"
+        echo "Running: $cmd"
+        eval $cmd
+        ;;
+    'autobuild')
+        echo 'Building the project one each source changes.'
+        cmd="${CONTAINER_START_CMD} ${AUTO_BUILD_CMD}"
+        echo "Running: $cmd"
+        eval $cmd
+        ;;
+    'test')
+        cmd="${CONTAINER_START_CMD} ${TEST_CMD}"
+        echo "Running: $cmd"
+        eval $cmd
+        ;;
     'run'|'')
-    echo "Running container ${CONTAINER_NAME} using:"
-    echo "  ${ROOT_DIR} as source directory (mapped to /home/saop/code)"
-    echo "  ${FIRERS_DATA} as data directory (mapped to /home/saop/data)"
-    echo ""
-    docker run -it \
-       -v ${ROOT_DIR}:/home/saop/code \
-       -v ${FIRERS_DATA}:/home/saop/data \
-       -e DISPLAY=${DISPLAY} \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
-       ${CONTAINER_NAME}
-    ;;
+        echo "Running container ${CONTAINER_NAME} using:"
+        echo "  ${ROOT_DIR} as source directory (mapped to /home/saop/code)"
+        echo "  ${FIRERS_DATA} as data directory (mapped to /home/saop/data)"
+        echo ""
+        eval ${CONTAINER_START_CMD}
+        ;;
     *)
-    echo "Error: Expected 'run', 'build' or '' as first parameter. Got: $1"
-    ;;
+        echo "Error: Expected 'make_container', 'run', 'build', 'autobuild', 'test' or '' as first parameter. Got: $1"
+        ;;
 esac
 
 
