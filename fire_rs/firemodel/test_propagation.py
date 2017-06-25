d82111c5415176ea07674723151f14445e4b52ab
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-06-25 10:33:06 +0200

    [fire-models] Set test area to a burnable one.

--- fire_rs/firemodel/test_propagation.py
+++ fire_rs/firemodel/test_propagation.py
@@ -5,7 +5,7 @@ import fire_rs.firemodel.propagation as propagation
 class TestPropagation(unittest.TestCase):
 
     def test_propagate(self):
-        env = propagation.Environment([[475060.0, 477060.0], [6200074.0, 6202074.0]], wind_speed=4.11, wind_dir=0)
-        prop = propagation.propagate(env, 10, 20)
+        env = propagation.Environment([[480060.0, 490060.0], [6210074.0, 6220074.0]], wind_speed=4.11, wind_dir=0)
+        prop = propagation.propagate(env, 10, 20, horizon=3*3600)
         # prop.plot(blocking=True)
 
