d538637383bee044387b45e4b30535ae861fd743
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-06-28 10:39:21 +0200

    [firemodel] Add large propagation test.

--- fire_rs/firemodel/test_propagation.py
+++ fire_rs/firemodel/test_propagation.py
@@ -4,8 +4,16 @@ import fire_rs.firemodel.propagation as propagation
 
 class TestPropagation(unittest.TestCase):
 
+
+    def setUp(self):
+        self.test_area = [[480060.0, 490060.0], [6210074.0, 6220074.0]]
+
     def test_propagate(self):
-        env = propagation.Environment([[480060.0, 490060.0], [6210074.0, 6220074.0]], wind_speed=4.11, wind_dir=0)
+        env = propagation.Environment(self.test_area, wind_speed=4.11, wind_dir=0)
         prop = propagation.propagate(env, 10, 20, horizon=3*3600)
         # prop.plot(blocking=True)
 
+    def test_propagate_full(self):
+        env = propagation.Environment(self.test_area, wind_speed=18.11, wind_dir=0)
+        prop = propagation.propagate(env, 10, 20)
+        # prop.plot(blocking=True)
