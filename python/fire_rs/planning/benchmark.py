63ea8838c64359ed71e4aba25681f7869c303866
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2017-10-27 16:53:49 +0200

    Recover the draw_flighttime_path function

--- python/fire_rs/planning/benchmark.py
+++ python/fire_rs/planning/benchmark.py
@@ -123,7 +123,7 @@ class PlanDisplayExtension(fire_rs.geodata.display.DisplayExtension):
         '''Bounds drawing methods to a GeoDataDisplayInstance.'''
         geodatadisplay.plan_trajectory = self.plan_trajectory
         geodatadisplay.draw_waypoints = types.MethodType(PlanDisplayExtension._draw_waypoints_extension, geodatadisplay)
-        geodatadisplay.draw_flighttime_path = types.MethodType(PlanDisplayExtension._draw_path_extension, geodatadisplay)
+        geodatadisplay.draw_flighttime_path = types.MethodType(PlanDisplayExtension._draw_flighttime_path_extension, geodatadisplay)
         geodatadisplay.draw_solid_path = types.MethodType(PlanDisplayExtension._draw_solid_path_extension, geodatadisplay)
         geodatadisplay.draw_segments = types.MethodType(PlanDisplayExtension._draw_segments_extension, geodatadisplay)
         geodatadisplay.draw_observedcells = types.MethodType(PlanDisplayExtension._draw_observedcells, geodatadisplay)
@@ -137,14 +137,11 @@ class PlanDisplayExtension(fire_rs.geodata.display.DisplayExtension):
         self._drawings.append(self.axis.scatter(x[::2], y[::2], s=7, c=color, marker='D'))
         self._drawings.append(self.axis.scatter(x[1::2], y[1::2], s=7, c=color, marker='>'))
 
-    def _draw_path_extension(self, *args, single_color: 'Optional[str]' = None,
-                             colorbar_time_range: 'Optional[Tuple[float, float]]' = None, **kwargs):
+    def _draw_flighttime_path_extension(self, *args, colorbar_time_range: 'Optional[Tuple[float, float]]' = None,
+                                        **kwargs):
         '''Draw trajectory in a GeoDataDisplay figure.
 
-        If the single_color argument is set, the trajectory will be displayed with a single color and
-        other color related options are ignored.
-
-        Otherwise the color of the trajectory will reflect the time taken by the trajectory.
+        The color of the trajectory will reflect the time taken by the trajectory.
         Optional argument colorbar_time_range may be a tuple of start and end times in seconds.
         If the Optional argument with_colorbar is set to True, a color bar will be displayed of the trajectory.
         '''
@@ -153,19 +150,16 @@ class PlanDisplayExtension(fire_rs.geodata.display.DisplayExtension):
         sampled_waypoints = self.plan_trajectory.sampled(step_size=5)
         x = [wp.x for wp in sampled_waypoints]
         y = [wp.y for wp in sampled_waypoints]
-        if single_color:
-            self._drawings.append(self.axis.scatter(x, y, s=1, edgecolors='none', c=single_color, zorder=2))
-        else:
-            color_range = np.linspace(self.plan_trajectory.start_time() / 60, self.plan_trajectory.end_time() / 60, len(x))
-            color_norm = matplotlib.colors.Normalize(vmin=color_range[0], vmax=color_range[-1])
-            if colorbar_time_range is not None:
-                color_norm = matplotlib.colors.Normalize(vmin=colorbar_time_range[0]/60, vmax=colorbar_time_range[1]/60)
-            self._drawings.append(self.axis.scatter(x, y, s=1, edgecolors='none', c=color_range,
-                                       norm=color_norm, cmap=matplotlib.cm.gist_rainbow, zorder=2))
-            if kwargs.get('with_colorbar', False):
-                cb = self._figure.colorbar(self._drawings[-1], ax=self.axis, shrink=0.65, aspect=20)
-                cb.set_label("Flight time [min]")
-                self._colorbars.append(cb)
+        color_range = np.linspace(self.plan_trajectory.start_time() / 60, self.plan_trajectory.end_time() / 60, len(x))
+        color_norm = matplotlib.colors.Normalize(vmin=color_range[0], vmax=color_range[-1])
+        if colorbar_time_range is not None:
+            color_norm = matplotlib.colors.Normalize(vmin=colorbar_time_range[0]/60, vmax=colorbar_time_range[1]/60)
+        self._drawings.append(self.axis.scatter(x, y, s=1, edgecolors='none', c=color_range,
+                                   norm=color_norm, cmap=matplotlib.cm.gist_rainbow, zorder=2))
+        if kwargs.get('with_colorbar', False):
+            cb = self._figure.colorbar(self._drawings[-1], ax=self.axis, shrink=0.65, aspect=20)
+            cb.set_label("Flight time [min]")
+            self._colorbars.append(cb)
 
     def _draw_solid_path_extension(self, *args, **kwargs):
         '''Draw trajectory in a GeoDataDisplay figure with solid color
@@ -217,7 +211,7 @@ def plot_plan(plan, geodatadisplay, time_range: 'Optional[Tuple[float, float]]' 
     colors = cycle(["red", "green", "blue", "black", "magenta"])
     for traj, color in zip(plan.trajectories(), colors):
         geodatadisplay.plan_trajectory = traj
-        geodatadisplay.draw_solid_path(color=color, colorbar_time_range=time_range, with_colorbar=True)
+        geodatadisplay.draw_solid_path(color=color)
         geodatadisplay.draw_segments(color=color)
     if show:
         geodatadisplay.axis.get_figure().show()
