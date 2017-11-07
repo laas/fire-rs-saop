5349902d0483e245e4b9cc2a678afd3f512ec764
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2017-11-07 13:36:35 +0100

    Refactor dubins3d tests

--- cpp/src/test/test_dubins.h
+++ cpp/src/test/test_dubins.h
@@ -33,111 +33,87 @@ using namespace std;
 static double r_min = 25; // m
 static double gamma_max = 0.1; // rad
 
-bool test_length_high_alt() {
-    cout << "::test_length_high_alt::" << endl;
+void test_helix_optimization_convergence() {
+    Waypoint3d orig {546003.90363695205, 6212215.7933926694, 514, 1.9718229235899096};
+    Waypoint3d dest {545600, 6213400, 100, 0};
 
-    Waypoint3d orig {100, 100, 0, M_PI_2};
-    Waypoint3d dest {0, 0, 200, 3 * M_PI_2};
+    Dubins3dPathLength path(orig, dest, 64.457751952217606, 0.10471975511965977); // Should not fail
+}
+
+void test_length_high_alt() {
+    Waypoint3d orig {100, 100,   0, M_PI_2};
+    Waypoint3d dest {  0,   0, 200, 3 * M_PI_2};
 
     Dubins3dPathLength path(orig, dest, r_min, gamma_max);
 
-    cout << orig << " -> " << dest << " => " << path.L << "xyz / " << path.L_2d << "xy" << endl;
-    return true;
+    BOOST_ASSERT(*path.goal_altitude == Dubins3dGoalAltitude::High);
 }
 
-bool test_length_medium_alt() {
-    cout << "::test_length_medium_alt::" << endl;
-
-    Waypoint3d orig {100, 100, 0, M_PI_2};
-    Waypoint3d dest {0, 0, 50, 3 * M_PI_2};
+void test_length_medium_alt() {
+    Waypoint3d orig {100, 100,  0, M_PI_2};
+    Waypoint3d dest {  0,   0, 25, 3 * M_PI_2};
 
     Dubins3dPathLength path(orig, dest, r_min, gamma_max);
 
-    cout << orig << " -> " << dest << " => " << path.L << "xyz / " << path.L_2d << "xy" << endl;
-    return true;
+    BOOST_ASSERT(*path.goal_altitude == Dubins3dGoalAltitude::Medium);
 }
 
-bool test_medium_alt_SSLS() {
-    cout << "::test_medium_alt_SSLS::" << endl;
-    Waypoint3d orig {100, 100, 0, M_PI_2};
-    Waypoint3d dest {0, 0, 25, 3 * M_PI_2};
+void test_medium_alt_SSLS() {
+    Waypoint3d orig {100, 100,  0, M_PI_2};
+    Waypoint3d dest {  0,   0, 25, 3 * M_PI_2};
 
     Dubins3dPathLength path(orig, dest, r_min, gamma_max);
 
-    BOOST_TEST(path.R>=0);
-
-    cout << orig << " -> " << dest << " => " << path.L << "xyz / " << path.L_2d << "xy" << endl;
-    return true;
+    BOOST_ASSERT(*path.goal_altitude == Dubins3dGoalAltitude::Medium);
+    BOOST_ASSERT(*path.configuration == Dubins3dPathType::SSLS);
 }
 
-bool test_length_low_alt() {
-    cout << "::test_length_low_alt::" << endl;
-
-    Waypoint3d orig {100, 100, 0, M_PI_2};
-    Waypoint3d dest {0, 0, 15, 3 * M_PI_2};
+void test_length_low_alt() {
+    Waypoint3d orig {100, 100,  0, M_PI_2};
+    Waypoint3d dest {  0,   0, 15, 3 * M_PI_2};
 
     Dubins3dPathLength path(orig, dest, r_min, gamma_max);
+    BOOST_ASSERT(*path.goal_altitude == Dubins3dGoalAltitude::Low);
 
-    cout << orig << " -> " << dest << " => " << path.L << "xyz / " << path.L_2d << "xy" << endl;
-    return true;
+    double orig_array[3] {orig.x, orig.y, orig.dir};
+    double dest_array[3] {dest.x, dest.y, dest.dir};
+    DubinsPath path2d;
+    dubins_init(orig_array, dest_array, r_min, &path2d);
+    BOOST_CHECK_CLOSE(path.L_2d, dubins_path_length(&path2d), 1);
 }
 
-bool test_triangleineq_flat() {
-    Waypoint3d a {0, 0, 0, 0};
+/* Triangle inequality should exist for low altitude paths */
+void test_triangleineq_flat() {
+    Waypoint3d a {  0,   0, 0, 0};
     Waypoint3d b {100, 100, 0, M_PI_2};
-    Waypoint3d c {0, 100, 0, M_PI};
+    Waypoint3d c {  0, 100, 0, M_PI};
     Dubins3dPathLength path_ab(a, b, r_min, gamma_max);
     Dubins3dPathLength path_bc(b, c, r_min, gamma_max);
     Dubins3dPathLength path_ac(a, c, r_min, gamma_max);
 
-    if (path_ab.configuration_2d && path_bc.configuration_2d && path_ac.configuration_2d) {
-        BOOST_TEST(path_ab.L + path_bc.L > path_ac.L);
-        BOOST_TEST(path_ab.L_2d + path_bc.L_2d > path_ac.L_2d);
-        return true;
-    }
+    BOOST_ASSERT(path_ab.configuration_2d && path_bc.configuration_2d && path_ac.configuration_2d);
 
-    return false;
+    BOOST_TEST(path_ab.L + path_bc.L > path_ac.L);
+    BOOST_TEST(path_ab.L_2d + path_bc.L_2d > path_ac.L_2d);
 }
 
-bool test_triangleineq_highest() {
-    Waypoint3d a {0, 0, 0, 0};
-    Waypoint3d b {100, 100, 500, M_PI_2};
-    Waypoint3d c {200, 100, 1000, M_PI};
-    Dubins3dPathLength path_ab(a, b, r_min, gamma_max);
-    Dubins3dPathLength path_bc(b, c, r_min, gamma_max);
-    Dubins3dPathLength path_ac(a, c, r_min, gamma_max);
-
-    if (path_ab.configuration_2d && path_bc.configuration_2d && path_ac.configuration_2d) {
-        BOOST_TEST(path_ab.L + path_bc.L > path_ac.L);
-        BOOST_TEST(path_ab.L_2d + path_bc.L_2d > path_ac.L_2d);
-        return true;
-    }
-
-    return false;
-}
-
-
-bool test_triangleineq_high() {
-    Waypoint3d a {0, 0, 0, 0};
-    Waypoint3d b {100, 100, 50, M_PI_2};
+/* Triangle inequality should NOT exist for high altitude paths */
+void test_triangleineq_high() {
+    Waypoint3d a {  0,   0,   0, 0};
+    Waypoint3d b {100, 100,  50, M_PI_2};
     Waypoint3d c {200, 100, 100, M_PI};
     Dubins3dPathLength path_ab(a, b, r_min, gamma_max);
     Dubins3dPathLength path_bc(b, c, r_min, gamma_max);
     Dubins3dPathLength path_ac(a, c, r_min, gamma_max);
 
-    if (path_ab.configuration_2d && path_bc.configuration_2d && path_ac.configuration_2d) {
-        BOOST_TEST(path_ab.L + path_bc.L > path_ac.L);
-        BOOST_TEST(path_ab.L_2d + path_bc.L_2d > path_ac.L_2d);
-        return true;
-    }
-
-    return false;
+    BOOST_CHECK_CLOSE(path_ab.L + path_bc.L, path_ac.L, 1);
+    BOOST_CHECK_CLOSE(path_ab.L_2d + path_bc.L_2d, path_ac.L_2d, 1);
 }
 
-bool test_length_flat() {
+void test_length_flat() {
     DubinsPath path2d;
     Waypoint3d orig {100, 100, 0, M_PI_2};
-    Waypoint3d dest {0, 0, 0, 3 * M_PI_2};
+    Waypoint3d dest {  0,   0, 0, 3 * M_PI_2};
 
     double orig_array[3] {orig.x, orig.y, orig.dir};
     double dest_array[3] {dest.x, dest.y, dest.dir};
@@ -145,11 +121,7 @@ bool test_length_flat() {
     Dubins3dPathLength path3d(orig, dest, r_min, gamma_max);
     dubins_init(orig_array, dest_array, r_min, &path2d);
 
-    cout << orig << " -> " << dest << " => " << path3d.L << "xyz / " << path3d.L_2d << "xy" << endl;
-    cout << dubins_path_length(&path2d) << " type=" << dubins_path_type(&path2d) << endl;
-
     BOOST_TEST(path3d.L == dubins_path_length(&path2d));
-    return path3d.L == dubins_path_length(&path2d);
 }
 
 test_suite* dubins_test_suite() {
@@ -161,7 +133,7 @@ test_suite* dubins_test_suite() {
     ts1->add(BOOST_TEST_CASE(&test_length_high_alt));
     ts1->add(BOOST_TEST_CASE(&test_length_flat));
     ts1->add(BOOST_TEST_CASE(&test_triangleineq_flat));
-    ts1->add(BOOST_TEST_CASE(&test_triangleineq_highest));
     ts1->add(BOOST_TEST_CASE(&test_triangleineq_high));
+    ts1->add(BOOST_TEST_CASE(&test_helix_optimization_convergence));
     return ts1;
 }
