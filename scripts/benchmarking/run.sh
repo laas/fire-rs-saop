73313a18bb7e9ca29ac6b0b8e1a744fddb92d352
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2017-11-17 11:19:38 +0100

    Fix wrong path for vns_conf.json in benchmarking

--- scripts/benchmarking/run.sh
+++ scripts/benchmarking/run.sh
@@ -6,8 +6,8 @@ bash docker_start.sh
 pwd
 bash docker_exec.sh "make build-release"
 
-bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=./vns_confs.json --vns=base --elevation=flat"
-bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=./vns_confs.json --vns=full --elevation=flat"
-bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=./vns_confs.json --vns=with_smoothing --elevation=flat"
+bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=base --elevation=flat"
+bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=full --elevation=flat"
+bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=with_smoothing --elevation=flat"
 
 docker rm -f saop
