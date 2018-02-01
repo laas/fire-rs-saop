578b1480fa7aba9ae2092429bc389e3a3cb36f50
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-02-01 13:56:27 +0100

    Call automated benchmark scripts with sh instead of bash

--- scripts/benchmarking/run.sh
+++ scripts/benchmarking/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # start a docker container in the background
-bash docker_start.sh
+sh docker_start.sh
 
 pwd
-bash docker_exec.sh "make build-release"
+sh docker_exec.sh "make build-release"
 
-bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=base --elevation=flat"
-bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=full --elevation=flat"
-bash docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=with_smoothing --elevation=flat"
+sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=base --elevation=flat"
+sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=full --elevation=flat"
+sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=with_smoothing --elevation=flat"
 
 docker rm -f saop
