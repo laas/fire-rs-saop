c0cb8634cc07012465d60e909083fd78e570969d
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-02-01 15:14:40 +0100

    Update automated benchmark run file

--- scripts/benchmarking/run.sh
+++ scripts/benchmarking/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # start a docker container in the background
-sh docker_start.sh
+sh docker_start.sh $1
 
 pwd
 sh docker_exec.sh "make build-release"
 
-sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=base --elevation=flat"
-sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=full --elevation=flat"
-sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --vns-conf=scripts/benchmarking/vns_confs.json --vns=with_smoothing --elevation=flat"
+sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --format svg --vns-conf=scripts/benchmarking/vns_confs.json --vns=base --elevation=flat"
+sh docker_exec.sh "python3 python/fire_rs/planning/benchmark.py --format svg --vns-conf=scripts/benchmarking/vns_confs.json --vns=full --elevation=flat"
 
 docker rm -f saop
