b0b8869412c7fc3079a3e77408bb787ed277f1d4
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-02-06 14:05:49 +0100

    Run benchmarks in separate containers

--- scripts/benchmarking/docker_exec.sh
+++ scripts/benchmarking/docker_exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# simple script to run a command in the "saop" container
+# simple script to run a command in the "saop_$1" container
 
-PARAMS="$@"
+PARAMS="${@:2}"
 
-docker exec -it --user=${USER_ID}:${GROUP_ID} saop /bin/bash -c "$PARAMS"
+docker exec -it --user=${USER_ID}:${GROUP_ID} saop_$1 /bin/bash -c "$PARAMS"
