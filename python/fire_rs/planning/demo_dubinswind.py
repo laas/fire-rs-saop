bd97263b5de86c2fad9cbfb9e9e715fae5c5f0de
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-02-23 15:01:36 +0100

    Update demo_dubinswind

--- python/fire_rs/planning/demo_dubinswind.py
+++ python/fire_rs/planning/demo_dubinswind.py
@@ -22,6 +22,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import fire_rs.uav_planning as up
@@ -58,52 +59,75 @@ def traj_wind_airframe(uav, orig, dest, wind):
 
 
 if __name__ == '__main__':
+    matplotlib.rcParams['font.family'] = 'sans-serif'
+    matplotlib.rcParams['text.usetex'] = True
 
-    uav_speed = 18.  # m/s
+    uav_speed = 10.  # m/s
     uav_max_turn_rate = 32. * np.pi / 180
     uav_max_pitch_angle = 6. / 180. * np.pi
-    wind = up.WindVector(-2, -2)
+    wind = up.WindVector(5, 0)
     no_wind = up.WindVector(0, 0)
     a_uav = up.UAV(uav_speed, uav_max_turn_rate, uav_max_pitch_angle)
 
+    wp_s, wp_e = up.Waypoint(0, 0, 0, 0), up.Waypoint(100, 0, 0, 0)
+    seg = up.Segment(wp_s, wp_e)
+    tr_time = a_uav.travel_time(seg, wind)
+    print(tr_time)
+    print(a_uav.travel_time(wp_s, wp_e, wind))
+
     fig = plt.figure()
     # Low altitude
-    ax = fig.add_subplot(111)
-    ax.set_xlim((-200, 100))
-    ax.set_ylim((-100, 200))
-    wp_ma = [(0, 0, 0, 0), (50, 0, 0, 0)]  # , (0, 100, 20, 0)]
-    for i in range(len(wp_ma) - 1):
-        ax.plot(*traj_wind_groundframe(a_uav, wp_ma[i], wp_ma[i + 1], no_wind), alpha=.66,
-                label="W/o wind")
-        ax.plot(*traj_wind_groundframe(a_uav, wp_ma[i], wp_ma[i + 1], wind), label="Ground frame")
-        ax.plot(*traj_wind_airframe(a_uav, wp_ma[i], wp_ma[i + 1], wind), linestyle='--',
-                label="Air frame")
+    ax = fig.add_subplot(111, aspect='equal')
+    ax.set_xlim((-55, 75))
+    ax.set_ylim((-55, 105))
 
-        ax.arrow(wp_ma[i][0], wp_ma[i][1], 4 * np.cos(wp_ma[i][2]), 4 * np.sin(wp_ma[i][2]),
+    ax.set_axis_off()  #Hide axes
+
+    wp_ma = [(-40, -50, 0, 0), (50, 50, 0, 3 * np.pi / 2)]  # , (0, 100, 20, 0)]
+    for i in range(len(wp_ma) - 1):
+        t_no_wind = traj_wind_groundframe(a_uav, wp_ma[i], wp_ma[i + 1], no_wind)
+        t_wind = traj_wind_groundframe(a_uav, wp_ma[i], wp_ma[i + 1], wind)
+        t_air = traj_wind_airframe(a_uav, wp_ma[i], wp_ma[i + 1], wind)
+        # ax.plot(*t_no_wind, label="No wind")
+        ax.plot(*t_wind, label="Ground frame")
+        ax.plot(*t_air, linestyle='--', label="Air frame")
+
+        ax.arrow(wp_ma[i][0], wp_ma[i][1], 4 * np.cos(wp_ma[i][3]), 4 * np.sin(wp_ma[i][3]),
                  fc="darkgreen", ec="darkgreen", head_width=4, head_length=4, zorder=100)
-        ax.arrow(wp_ma[i + 1][0], wp_ma[i + 1][1], 4 * np.cos(wp_ma[i + 1][2]),
-                 4 * np.sin(wp_ma[i + 1][2]),
+        ax.arrow(wp_ma[i + 1][0], wp_ma[i + 1][1], 4 * np.cos(wp_ma[i + 1][3]),
+                 4 * np.sin(wp_ma[i + 1][3]),
                  fc="r", ec="r", head_width=4, head_length=4, zorder=100)
-        ax.scatter(wp_ma[i][0], wp_ma[i][1], c='darkgreen', zorder=101)  # Start
-        ax.scatter(wp_ma[i + 1][0], wp_ma[i + 1][1], c='r', zorder=101)  # End
+        ax.scatter(wp_ma[i][0], wp_ma[i][1], c='darkgreen', zorder=100)  # Start
+        ax.scatter(wp_ma[i + 1][0], wp_ma[i + 1][1], c='r', zorder=100)  # End
         ax.annotate('start', xy=(wp_ma[i][0], wp_ma[i][1]), xytext=(wp_ma[i][0], wp_ma[i][1] - 15))
         ax.annotate('end', xy=(wp_ma[i + 1][0], wp_ma[i + 1][1]),
-                    xytext=(wp_ma[i + 1][0], wp_ma[i + 1][1] + 10))
+                    xytext=(wp_ma[i + 1][0], wp_ma[i + 1][1] - 15))
 
-        wind_location = (-150, 150)
-        ax.annotate('wind ' + format(wind.speed(), '.2f') + 'm/s',
+        wind_location = (-20, 90)
+        ax.annotate("wind",  # + " " + format(wind.speed(), '.0f') + 'm/s',
                     xy=(wind_location[0], wind_location[1]),
-                    xytext=(wind_location[0] - 30 * np.cos(wind.dir()),
-                            wind_location[1] - 30 * np.cos(wind.dir())))
+                    xytext=(wind_location[0] - 0 * np.cos(wind.dir()),
+                            wind_location[1] - 10 * np.cos(wind.dir())))
         ax.arrow(wind_location[0], wind_location[1],
                  wind.speed() * np.cos(wind.dir()),
                  wind.speed() * np.sin(wind.dir()),
-                 fc="k", ec="k", head_width=5, head_length=5, zorder=100)
+                 fc="k", ec="k", head_width=4, head_length=5, zorder=100)
 
-    plt.xlabel("x")
-    plt.ylabel("y")
+        # virtual target
+        ax.scatter(t_air[0][-1], t_air[1][-1], c='k', zorder=100)  # vt
+        ax.annotate('virtual\ntarget', xy=(t_air[0][-1], t_air[1][-1]),
+                    xytext=(t_air[0][-1] - 10, t_air[1][-1] - 20))
+        ax.annotate("", xy=(t_wind[0][-1], t_wind[1][-1]), xytext=(t_air[0][-1], t_air[1][-1]),
+                    arrowprops=dict(arrowstyle="->"), zorder=101)
+
+    # plt.xlabel("x")
+    # plt.ylabel("y")
     plt.legend(loc='upper right')
-    plt.title("Optimal trajectory of a UAV under the presence of constant wind")
+    # plt.title("Optimal trajectory of a UAV under the presence of constant wind")
 
+    fig.set_size_inches(6, 4.5)
+    fig.savefig("dubinswind-method.pdf", dpi=300, bbox_inches='tight')
+    fig.savefig("dubinswind-method.svg", dpi=300, bbox_inches='tight')
     plt.show(block=True)
+
     print("end")
