e27778d71d299afdc252fd5d8462ecb8134607c6
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-04-05 13:42:45 +0200

    Update generate_windy_scenario

--- python/fire_rs/planning/benchmark.py
+++ python/fire_rs/planning/benchmark.py
@@ -323,20 +323,17 @@ def generate_scenario_singlefire_singleuav():
 
 def generate_windy_scenario():
     # 9 by 7 km area
-    area = Area(480060.0, 489060.0, 6210074.0, 6217074.0)
-    uav_speed = 18.  # m/s
-    uav_max_pitch_angle = 6. / 180. * np.pi
-    uav_max_turn_rate = 32. * np.pi / 180
+    area = Area(480000.0, 485000.0, 6210000.0, 6215000.0)
     uav_bases = [  # four corners of the map
         Waypoint(area.xmin + 100, area.ymin + 100, 0, 0),
-        Waypoint(area.xmin + 100, area.ymax - 100, 0, 0),
-        Waypoint(area.xmax - 100, area.ymin + 100, 0, 0),
-        Waypoint(area.xmax - 100, area.ymax - 100, 0, 0)
+        Waypoint(area.xmin + 100, area.ymin + 100, 0, 0),
+        Waypoint(area.xmin + 100, area.ymin + 100, 0, 0),
+        Waypoint(area.xmin + 100, area.ymin + 100, 0, 0)
     ]
 
     wind_speed = 5. # in m/s
     wind_dir = 0.  # random.random() * 2 * np.pi
-    num_ignitions = random.randint(1, 3)
+    num_ignitions = random.randint(1, 4)
     ignitions = [TimedPoint(random.uniform(area.xmin, area.xmax),
                             random.uniform(area.ymin, area.ymax),
                             random.uniform(0, 3000))
@@ -345,12 +342,11 @@ def generate_windy_scenario():
     # start once all fires are ignited
     start = max([igni.time for igni in ignitions])
 
-    num_flights = random.randint(1, 3)
+    num_flights = random.randint(1, 4)
     flights = []
     for i in range(num_flights):
-        uav_start = random.uniform(start, start + 4000.)
-        max_flight_time = random.uniform(5000, 6000)
-        uav = UAVConf(uav_speed, uav_max_turn_rate, uav_max_pitch_angle, max_flight_time)
+        uav_start = random.uniform(start+3000, start + 3001.)
+        uav = UAVConf.X8()
         flights.append(FlightConf(uav, uav_start, random.choice(uav_bases), None,
                                   (wind_speed * np.cos(wind_dir), wind_speed * np.sin(wind_dir))))
 
