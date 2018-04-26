0bf9624916c8e705edfb1d46f54ce651707c2c04
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-04-26 15:38:19 +0200

    Separate IMC comm and SAOP Plan server code in different files

---
+++ cpp/src/neptus/main.cpp
@@ -0,0 +1,92 @@
+/* Copyright (c) 2017-2018, CNRS-LAAS
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+#include "imc_comm.hpp"
+#include "saop_server.hpp"
+
+void user_input_loop(std::shared_ptr<SAOP::neptus::IMCCommManager> imc_comm) {
+    bool exit = false;
+    auto plan_spec_message = SAOP::neptus::PlanSpecificationFactory::make_message();
+
+    while (!exit) {
+        std::cout << "Send message: ";
+        std::string u_input;
+        std::cin >> u_input;
+        std::cout << std::endl;
+        if (u_input == "e") {
+            exit = !exit;
+        } else if (u_input == "lp") {
+            auto a_plan = SAOP::neptus::PlanControlFactory::make_load_plan_message(plan_spec_message, 12345);
+            imc_comm->send(std::unique_ptr<IMC::Message>(new IMC::PlanControl(std::move(a_plan))));
+        } else if (u_input == "sp") {
+            auto a_plan = SAOP::neptus::PlanControlFactory::make_start_plan_message(plan_spec_message.plan_id);
+            imc_comm->send(std::unique_ptr<IMC::Message>(new IMC::PlanControl(std::move(a_plan))));
+        } else {
+            std::cout << "Wrong command" << std::endl;
+        }
+    }
+}
+
+int main() {
+    std::function<void(std::unique_ptr<IMC::Message>)> recv_handler = [](std::unique_ptr<IMC::Message> m) {
+        std::cout << "Received: " << m->getSource() << " " << static_cast<uint>(m->getSourceEntity()) << " "
+                  << m->getDestination() << " " << static_cast<uint>(m->getDestinationEntity()) << std::endl;
+    };
+
+    std::function<void(std::unique_ptr<IMC::EstimatedState>)> recv_estimatedstate = [](
+            std::unique_ptr<IMC::EstimatedState> m) {
+        std::cout << "Received: " << m->getSource() << " " << static_cast<uint>(m->getSourceEntity()) << " "
+                  << m->getDestination() << " " << static_cast<uint>(m->getDestinationEntity()) << std::endl;
+    };
+
+    std::function<void(std::unique_ptr<IMC::PlanControl>)> recv_plancontrol = [](
+            std::unique_ptr<IMC::PlanControl> m) {
+        m->toJSON(std::cout);
+        std::cout << std::endl;
+    };
+
+    std::function<void(std::unique_ptr<IMC::PlanControlState>)> recv_plancontrolstate = [](
+            std::unique_ptr<IMC::PlanControlState> m) {
+        m->toJSON(std::cout);
+        std::cout << std::endl;
+    };
+
+    std::shared_ptr<SAOP::neptus::IMCCommManager> imc_comm = std::make_shared<SAOP::neptus::IMCCommManager>();
+
+    // Start IMCCommManager in a different thread
+    auto t = std::thread(std::bind(&SAOP::neptus::IMCCommManager::run, std::ref(imc_comm)));
+
+    // Demo EstimatedState handler
+    imc_comm->bind<IMC::EstimatedState>(recv_estimatedstate);
+    imc_comm->bind<IMC::PlanControl>(recv_plancontrol);
+    imc_comm->bind<IMC::PlanControlState>(recv_plancontrolstate);
+
+    // Send a demo plan
+    auto plan_spec_message = SAOP::neptus::PlanSpecificationFactory::make_message();
+    auto a_plan = SAOP::neptus::PlanControlFactory::make_load_plan_message(plan_spec_message, 12345);
+    imc_comm->send(std::unique_ptr<IMC::Message>(new IMC::PlanControl(std::move(a_plan))));
+
+    user_input_loop(imc_comm);
+
+}
--- cpp/src/neptus/saop_server.hpp
+++ cpp/src/neptus/saop_server.hpp
@@ -37,12 +37,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 #include <thread>
 #include <vector>
 
-#include <boost/asio.hpp>
-
-#include "../../IMC/Base/ByteBuffer.hpp"
 #include "../../IMC/Base/Message.hpp"
-#include "../../IMC/Base/Packet.hpp"
-#include "../../IMC/Base/Parser.hpp"
 #include "../../IMC/Spec/EstimatedState.hpp"
 #include "../../IMC/Spec/Heartbeat.hpp"
 #include "../../IMC/Spec/PlanControl.hpp"
@@ -50,128 +45,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 #include "../vns/plan.hpp"
 
+#include "imc_comm.hpp"
 #include "imc_message_factories.hpp"
 
 namespace SAOP {
 
-    template<typename T>
-    class SharedQueue {
-        std::queue<T> q;
-        std::mutex m;
-
-    public:
-        SharedQueue<T>() = default;
-
-        /* Retrieve the first element
-         * returns true if the queue wasn't empty and false otherwise.*/
-        bool pop(T& x) {
-            std::lock_guard<std::mutex> lock(m);
-            if (!q.empty()) {
-                x = std::move(q.front());
-                q.pop();
-                return true;
-            }
-            return false;
-        }
-
-        /*Put an element at the end of the queue*/
-        void push(T&& x) {
-            std::lock_guard<std::mutex> lock(m);
-            q.push(std::move(x));
-        }
-    };
-
     namespace neptus {
-
-        typedef SharedQueue<std::unique_ptr<IMC::Message>> IMCMessageQueue;
-
-        using boost::asio::ip::tcp;
-
-        class IMCTransportTCP {
-            unsigned short port;
-
-            std::function<void(std::unique_ptr<IMC::Message>)> recv_handler;
-            std::shared_ptr<IMCMessageQueue> send_q;
-
-        public:
-            explicit IMCTransportTCP(unsigned short port)
-                    : port(port), recv_handler(nullptr), send_q(std::make_shared<IMCMessageQueue>()) {}
-
-            IMCTransportTCP(unsigned short port,
-                            std::function<void(std::unique_ptr<IMC::Message>)> recv_handler)
-                    : port(port), recv_handler(std::move(recv_handler)), send_q(std::make_shared<IMCMessageQueue>()) {}
-
-            void run();
-
-            void session(std::shared_ptr<tcp::socket> sock);
-
-            /* Set function the function to be called on message reception */
-            void set_recv_handler(std::function<void(std::unique_ptr<IMC::Message>)> a_recv_handler) {
-                recv_handler = std::move(a_recv_handler);
-            }
-
-            /* Enqueue message to be sent */
-            void send(std::unique_ptr<IMC::Message> message) {
-                send_q->push(std::move(message));
-            }
-        };
-
-        /* TODO: Enable shared_from_this ? */
-        class IMCCommManager {
-            IMCTransportTCP tcp_server;
-            std::shared_ptr<IMCMessageQueue> recv_q;
-            std::unordered_map<size_t, std::function<void(std::unique_ptr<IMC::Message>)>> message_bindings;
-
-            std::unique_ptr<std::mutex> binding_mtx = std::unique_ptr<std::mutex>(new std::mutex());
-
-        public:
-            IMCCommManager() :
-                    tcp_server(IMCTransportTCP(8888)),
-                    recv_q(std::make_shared<SAOP::neptus::IMCMessageQueue>()) {}
-
-            explicit IMCCommManager(IMCTransportTCP tcp_server) :
-                    tcp_server(std::move(tcp_server)),
-                    recv_q(std::make_shared<SAOP::neptus::IMCMessageQueue>()) {}
-
-            void run();
-
-            /* Enqueue message to be sent */
-            void send(std::unique_ptr<IMC::Message> message) {
-                tcp_server.send(std::move(message));
-            }
-
-            /*Bind a message id to a handler function*/
-            template<typename M>
-            void bind(std::function<void(std::unique_ptr<M>)> message_handler) {
-                // std::function arguments cannot be casted. We define a wrapper instead
-                std::function<void(std::unique_ptr<IMC::Message>)> g_func = [message_handler](
-                        std::unique_ptr<IMC::Message> m) {
-                    message_handler(std::unique_ptr<M>(static_cast<M*>(m.release())));
-
-                };
-
-                std::unique_lock<std::mutex> lock(*binding_mtx);
-                if (message_bindings.count(M::getIdStatic()) > 0) {
-                    std::cerr << "Replacing existing binding for "
-                              << IMC::Factory::getAbbrevFromId(M::getIdStatic())
-                              << std::endl;
-                }
-                bind(M::getIdStatic(), g_func);
-            }
-
-        private:
-            /* Bind a message id to a handler function */
-            void bind(size_t id, std::function<void(std::unique_ptr<IMC::Message>)> message_handler) {
-                message_bindings[id] = std::move(message_handler);
-            }
-
-            void message_dispatching_loop();
-
-            void message_inbox(std::unique_ptr<IMC::Message> m) {
-                recv_q->push(std::move(m));
-            }
-
-        };
 
         enum class PlanExecutionState {
             Executing, // Plan is still running
