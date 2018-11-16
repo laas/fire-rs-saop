5127254c0701e2e4f303c9e70ead44fc7f22b387
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-11-16 17:06:54 +0100

    Update CCU bridge node

--- supersaop/src/ccu_bridge.py
+++ supersaop/src/ccu_bridge.py
@@ -11,11 +11,12 @@ import matplotlib
 matplotlib.use('Gtk3Agg')
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Header
 from geometry_msgs.msg import Point
 
 from supersaop.msg import ElevationMap, Euler, PoseEuler, PredictedWildfireMap, PropagateCmd, \
-    Raster, RasterMetaData, WildfireMap, MeanWindStamped, SurfaceWindMap, Timed2DPointStamped, Plan
+    Raster, RasterMetaData, WildfireMap, MeanWindStamped, SurfaceWindMap, Timed2DPointStamped, \
+    Plan, VehicleState, Velocity
 
 from fire_rs.geodata.display import GeoDataDisplay
 from fire_rs.monitoring.supersaop import NeptusBridge
@@ -38,6 +39,7 @@ class CCUBridgeNode:
 
         self.ccu = NeptusBridge(logging.getLogger(__name__))
         self.ccu.set_uav_state_callback(self.on_uav_state_from_neptus)
+        # self.ccu.set_trajectory_state_callback(self.on_trajectory_state_from_neptus)
 
         self._known_uavs = set()
         self._known_uavs.add('x8-06')
@@ -46,21 +48,32 @@ class CCUBridgeNode:
         self.dict_state_msg = {uav: None for uav in self._known_uavs}
 
         self.pub_dict_state = {uav: rospy.Publisher("/".join((ros_name_for(uav), 'state')),
-                                                    PoseEuler, queue_size=10) for uav in
+                                                    VehicleState, queue_size=10) for uav in
                                self._known_uavs}  # type: ty.Mapping[rospy.Publisher]
 
         self.current_saop_plan = None
-        self.sub_saop_plan = rospy.Subscriber("plan", Plan, queue_size=10)
+        self.sub_saop_plan = rospy.Subscriber("plan", Plan,  self.on_saop_plan, queue_size=10)
+
+    def on_saop_plan(self, msg: Plan):
+        t = serialization.saop_trajectories_from_plan_msg(msg)
+        self.ccu.start_trajectory(t[0], "demo_traj", "x8-06")
 
     def on_uav_state_from_neptus(self, **kwargs):
         if kwargs['uav'] in self._known_uavs:
             with self.dict_state_lock:
                 # kwargs['uav'] = rospy.Publisher("/".join((ros_name_for(kwargs['uav']), 'pose')),
                 #                                 PoseEuler, queue_size=10)
-                pose_msg = PoseEuler(position=Point(kwargs['x'], kwargs['y'], kwargs['z']),
-                                     orientation=Euler(phi=kwargs['phi'], theta=kwargs['theta'],
-                                                       psi=kwargs['psi']))
+                position_msg = Point(kwargs['x'], kwargs['y'], kwargs['z'])
+                orientation_msg = Euler(phi=kwargs['phi'], theta=kwargs['theta'],
+                                        psi=kwargs['psi'])
+                velocity_msg = Velocity(kwargs['vx'], kwargs['vy'], kwargs['vz'])
+                pose_msg = VehicleState(header=Header(stamp=rospy.Time.from_sec(kwargs['time'])),
+                                        name=kwargs['uav'], position=position_msg,
+                                        orientation=orientation_msg, ground_velocity=velocity_msg)
                 self.dict_state_msg[kwargs['uav']] = pose_msg
+
+    # def on_trajectory_state_from_neptus(self, **kwargs):
+    #     print(kwargs)
 
     def publish_state(self):
         """ Publish retrieved information from Neptus into ROS topics"""
