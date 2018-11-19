040f7435cee9f8b40c90c0f32b7753d5ed1a0b3e
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2018-11-19 15:25:15 +0100

    Stop using the thradpool for message delivery

--- cpp/src/neptus/imc_comm.cpp
+++ cpp/src/neptus/imc_comm.cpp
@@ -147,13 +147,14 @@ namespace SAOP {
                                       << "to(" << m->getDestination() << ", "
                                       << static_cast<uint>(m->getDestinationEntity()) << ")"
                                       << std::endl;
-                            auto m_raw = m.get();
-                            m.release();
-                            // variables can be moved into lambdas only from C++14 onwards, not C++11
-                            thp.enqueue([hnld_fun, m_raw] {
-                                std::unique_ptr<IMC::Message> mm = std::unique_ptr<IMC::Message>(m_raw);
-                                hnld_fun(std::move(mm));
-                            });
+                            hnld_fun(std::move(m));
+//                            auto m_raw = m.get();
+//                            m.release();
+//                            // variables can be moved into lambdas only from C++14 onwards, not C++11
+//                            thp.enqueue([hnld_fun, m_raw] {
+//                                std::unique_ptr<IMC::Message> mm = std::unique_ptr<IMC::Message>(m_raw);
+//                                hnld_fun(std::move(mm));
+//                            });
                         } catch (const std::out_of_range& e) {
                             BOOST_LOG_TRIVIAL(warning) << "Unhandled IMC message " << m->getName()
                                       << "(" << static_cast<uint>(m->getId()) << "): "
