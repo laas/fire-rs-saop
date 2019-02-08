e0efab2fa6a9433c833717e066ee34efdbf1b582
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2019-02-08 15:56:20 +0100

    Add method to Perimeter to obtain the area inside it

--- python/fire_rs/geodata/wildfire.py
+++ python/fire_rs/geodata/wildfire.py
@@ -41,10 +41,15 @@ class Perimeter:
         self._layer = layer
         self._empty_val = perimeter_background
 
-        self._perimeter_array, self._cells, self._n_contours = compute_perimeter(
+        self._perimeter_array, self._cells, self._contour = _compute_perimeter(
             self._wildfire, self._threshold, layer=self._layer, empty_val=self._empty_val)
 
+        if self._contour:
+            for cont in self._contour:
+                skimage.measure.grid_points_in_poly(self._wildfire.data.shape, cont)
+
         self._perimeter_geodata = None
+        self._area_array = None
 
     @property
     def array(self) -> np.ndarray:
@@ -57,7 +62,7 @@ class Perimeter:
     @property
     def count(self) -> int:
         """Number of indepedent perimeters"""
-        return self._n_contours
+        return len(self._contour)
 
     @property
     def geodata(self) -> fire_rs.geodata.geo_data.GeoData:
@@ -66,20 +71,31 @@ class Perimeter:
                                                            dtype=self._wildfire.data.dtype)
         return self._perimeter_geodata
 
+    @property
+    def area_array(self):
+        """Mask of cells inside the perimeter"""
+        if self._area_array is None:
+            if self._contour:
+                self._area_array = skimage.measure.grid_points_in_poly(self._wildfire.data.shape,
+                                                                       self._contour[0])
+                for cont in self._contour[1:]:
+                    self._area_array += skimage.measure.grid_points_in_poly(
+                        self._wildfire.data.shape, cont)
+        return self._area_array
+
 
 _compute_perimeter_output_type = ty.Tuple[
-    np.ndarray, ty.MutableMapping[ty.Tuple[int, int], np.float64], int]
+    np.ndarray, ty.MutableMapping[ty.Tuple[int, int], np.float64], ty.List[np.ndarray]]
 
 
-def compute_perimeter(wildfire: fire_rs.geodata.geo_data.GeoData, threshold: float,
-                      layer: str = 'ignition', empty_val=np.inf) -> _compute_perimeter_output_type:
+def _compute_perimeter(wildfire: fire_rs.geodata.geo_data.GeoData, threshold: float,
+                       layer: str = 'ignition', empty_val=np.inf) -> _compute_perimeter_output_type:
     """Extract the perimeter given by threshold from a fire map."""
 
     array = np.ones(wildfire.data.shape, dtype=np.float64) * empty_val
     cells = {}
 
     contours = skimage.measure.find_contours(wildfire.data[layer], threshold)
-    n_contours = len(contours)
 
     for contour in contours:
         prev_edge = contour[0]
@@ -96,7 +112,7 @@ def compute_perimeter(wildfire: fire_rs.geodata.geo_data.GeoData, threshold: flo
 
             prev_edge = edge
 
-    return array, cells, n_contours
+    return array, cells, contours
 
 
 def interpolate(x, y, z, shape, function='thin_plate') -> np.ndarray:
