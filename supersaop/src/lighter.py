16d1f605b3fdc5e1a821f3eb13c2f5372a91c658
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2019-05-16 15:57:49 +0200

    Define demo 2 ignition point and extent

--- supersaop/src/lighter.py
+++ supersaop/src/lighter.py
@@ -243,7 +243,36 @@ if __name__ == '__main__':
                 (w_starter.set_x8_06_wind, (3.0, 0.0)),
                 (w_starter.propagate, None)
             ]
+        elif location == "demo2-2":
+            wind = (3, 3 * np.pi / 2)
+            environment = fire_rs.firemodel.propagation.Environment(
+                area, wind[0], wind[1], fire_rs.geodata.environment.World(
+                    **world_paths,
+                    landcover_to_fuel_remap=fire_rs.geodata.environment.EVERYTHING_FUELMODEL_REMAP))
+            rw = fire_rs.simulation.wildfire.RealWildfire(
+                datetime.datetime.fromtimestamp((rospy.Time.now() - one_hour).to_sec()),
+                environment)
 
+            ignitions = [np.array([536043.0, 4570950.0])]
+
+            ignitions_cell = [rw.fire_map.array_index(p) for p in ignitions]
+
+            actions = [
+                (w_starter.save_dem_of_firemap, ((environment,))),
+                (rw.ignite, (ignitions[0],)),
+                (rw.propagate, (datetime.timedelta(minutes=180.),)),
+                (w_starter.notify_wind, (wind[0], wind[1])),
+                (w_starter.notify_alarm_point,
+                 (ignitions[0],
+                  lambda: rospy.Time.from_sec(rw.fire_map["ignition"][ignitions_cell[0]]))),
+                (w_starter.notify_alarm_map,
+                 (lambda: rw.perimeter(rospy.Time.now().to_sec() - 15 * 60).geodata,)),
+                # (w_starter.notify_alarm_map,
+                #  (lambda: rw.perimeter(rospy.Time.now().to_sec() - 60 * 60).geodata,)),
+                (w_starter.publish_real_fire, (rw,)),
+                (w_starter.set_x8_06_wind, (wind[0], wind[1])),
+                (w_starter.propagate, None)
+            ]
         # elif location == "porto":
         #     actions = [
         #         (w_starter.notify_alarm_point,
