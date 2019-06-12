ed50a5268fa80aaca1c6e35f3216d3bbdddbd004
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2019-06-12 10:04:27 +0200

    Update the Dockerfile so it can run recent versions of saop
     - Use ubuntu 18.04
     - Build windninja
     - Clone saop master during build
     - Update docker/run.sh

--- docker/run.bash
+++ docker/run.bash
@@ -6,13 +6,19 @@
 # When invoked with 'start', the container will be started to give a bash prompt in the container
 
 DOCKER_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))"
-ROOT_DIR="$(realpath $(dirname ${DOCKER_DIR}))"
 
+# X11 authorization stuff
 XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-CONTAINER_NAME='saop'
+CONTAINER_NAME='fire-rs-saop'
 
-CONTAINER_START_CMD="docker run -it --user=saop:saop --cap-add=SYS_PTRACE -v ${ROOT_DIR}:/home/saop/code:z -v ${FIRERS_DATA}:/home/saop/data:z -e DISPLAY=${DISPLAY} -v ${XSOCK} ${CONTAINER_NAME}"
+CONTAINER_START_CMD="docker run -it --user=saop:saop --cap-add=SYS_PTRACE \
+--volume ${FIRERS_DATA}:/home/saop/data:z --env DISPLAY=${DISPLAY} \
+--volume=$XSOCK:$XSOCK:rw --volume=$XAUTH:$XAUTH:rw --env="XAUTHORITY=${XAUTH}" \
+${CONTAINER_NAME}"
 
 USER_UID="$(id -u)"
 USER_GROUP="$(id -g)"
@@ -22,23 +28,24 @@ BUILD_OPTIONS="--build-arg USER_UID=${USER_UID} --build-arg USER_GROUP=${USER_GR
 case $1 in
     'build')
         echo "Building container $CONTAINER_NAME from directory $DOCKER_DIR"
+        echo "Cloning github laas:fire-rs-saop master as source directory (/home/saop/code)"
         echo ""
         docker build ${BUILD_OPTIONS} ${CONTAINER_NAME} ${DOCKER_DIR}
         ;;
     'rebuild')
         echo "Building container $CONTAINER_NAME from directory $DOCKER_DIR"
+        echo "Cloning github laas:fire-rs-saop master as source directory (/home/saop/code)"
         echo ""
         docker build --no-cache=true ${BUILD_OPTIONS} ${CONTAINER_NAME} ${DOCKER_DIR}
         ;;
     'start'|'')
         echo "Running container ${CONTAINER_NAME} using:"
-        echo "  ${ROOT_DIR} as source directory (mapped to /home/saop/code)"
-        echo "  ${FIRERS_DATA} as data directory (mapped to /home/saop/data)"
+        echo "{FIRERS_DATA} as data directory (mapped to /home/saop/data)"
         echo ""
         echo ${CONTAINER_START_CMD}
         eval ${CONTAINER_START_CMD}
         ;;
     *)
-        echo "Error: Expected 'make_container', 'start' or 'build' as parameter. Got: $1"
+        echo "Error: Expected 'build', 'rebuild' or 'start' as parameter. Got: $1"
         ;;
 esac
