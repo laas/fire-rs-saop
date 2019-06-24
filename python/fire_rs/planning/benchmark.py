6112f3e77e4501294bcf138a5f93c8aca511561e
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2019-06-24 17:26:54 +0200

    Use fire-rs-data repository for CI and for the default settings in docker

--- python/fire_rs/planning/benchmark.py
+++ python/fire_rs/planning/benchmark.py
@@ -431,7 +431,7 @@ def generate_nowind_scenario():
     return scenario
 
 
-def generate_scenario():
+def generate_scenario_lambert93():
     # 9 by 7 km area
     area = Area(480060.0, 489060.0, 6210074.0, 6217074.0)
     uav_speed = 18.  # m/s
@@ -468,7 +468,45 @@ def generate_scenario():
     return scenario
 
 
+def generate_scenario():
+    # 9 by 7 km area
+    area = Area(532000.0, 540000.0, 4567000.0, 4575000.0)
+    uav_speed = 18.  # m/s
+    uav_max_pitch_angle = 6. / 180. * np.pi
+    uav_max_turn_rate = 32. * np.pi / 180
+    uav_bases = [  # four corners of the map
+        Waypoint(area.xmin + 100, area.ymin + 100, 0, 0),
+        Waypoint(area.xmin + 100, area.ymax - 100, 0, 0),
+        Waypoint(area.xmax - 100, area.ymin + 100, 0, 0),
+        Waypoint(area.xmax - 100, area.ymax - 100, 0, 0)
+    ]
+
+    wind_speed = 15.  # random.uniform(10., 20.)  # wind speed in [10,20] km/h
+    wind_dir = 0.  # random.random() * 2 * np.pi
+    num_ignitions = random.randint(1, 3)
+    ignitions = [TimedPoint(random.uniform(area.xmin, area.xmax),
+                            random.uniform(area.ymin, area.ymax),
+                            random.uniform(0, 3000))
+                 for i in range(num_ignitions)]
+
+    # start once all fires are ignited
+    start = max([igni.time for igni in ignitions])
+
+    num_flights = random.randint(1, 3)
+    flights = []
+    for i in range(num_flights):
+        uav_start = random.uniform(start, start + 4000.)
+        max_flight_time = random.uniform(500, 1200)
+        uav = UAVConf("x8-06", uav_speed, uav_max_turn_rate, uav_max_pitch_angle, max_flight_time)
+        flights.append(FlightConf(uav, uav_start, random.choice(uav_bases)))
+
+    scenario = Scenario(((area.xmin, area.xmax), (area.ymin, area.ymax)),
+                        wind_speed, wind_dir, ignitions, flights)
+    return scenario
+
+
 scenario_factory_funcs = {'default': generate_scenario,
+                          'default_lambert93': generate_scenario_lambert93,
                           'utility_test': generate_scenario_utility_test,
                           'singlefire_singleuav': generate_scenario_singlefire_singleuav,
                           'singlefire_singleuav_shortrange': generate_scenario_singlefire_singleuav_shortrange,
