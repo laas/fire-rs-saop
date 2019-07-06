97cbd66cc45a8d6df257e6213149d50519f4cb1e
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2019-07-06 10:48:32 +0200

    Update demo_propagation_highres

--- python/fire_rs/demo_propagation_highres.py
+++ python/fire_rs/demo_propagation_highres.py
@@ -1,5 +1,6 @@
 """High precision wildfire simulation"""
 
+import datetime
 import os
 
 import numpy as np
@@ -11,29 +12,33 @@ import fire_rs.geodata.display as display
 from fire_rs.geodata.geo_data import GeoData
 from fire_rs.firemodel.propagation import Environment, FirePropagation, TimedPoint
 
-FIRERS_DATA_FOLDER = '/home/rbailonr/firers_data_5m'
+FIRERS_DATA_FOLDER = '/home/rbailonr/src/fire-rs-data-private/hautegaronne_lambert93'
 FIRERS_DEM_DATA = os.path.join(FIRERS_DATA_FOLDER,
-                               'dem/RGEALTI_MNT_5M_ASC_LAMB93_IGN69')
+                               'dem/')
 FIRERS_WIND_DATA = os.path.join(FIRERS_DATA_FOLDER,
-                                'wind/RGEALTI_MNT_5M_ASC_LAMB93_IGN69')
+                                'wind/')
 FIRERS_LANDCOVER_DATA = os.path.join(FIRERS_DATA_FOLDER,
-                                     'landcover/RGEALTI_MNT_5M_ASC_LAMB93_IGN69')
+                                     'landcover/')
 
 the_world = g_environment.World(elevation_path=FIRERS_DEM_DATA,
                                 wind_path=FIRERS_WIND_DATA,
-                                landcover_path=FIRERS_LANDCOVER_DATA, wind_mesh_resolution='fine')
+                                landcover_path=FIRERS_LANDCOVER_DATA, wind_mesh_resolution='fine',
+                                landcover_to_fuel_remap=g_environment.EVERYTHING_FUELMODEL_REMAP)
 the_world.dem_wind_tile_split = 1
 
-fire_env = Environment(((479998.0 - 2000, 484997.0 + 2000), (6210003.0 - 2000, 6215002.0 + 2000)),
-                       5, 0, the_world)
+fire_env = Environment(((479998.0+2000, 484997.0-2000), (6210003.0+2000, 6215002.0-2000)),
+                       10, 0, the_world)
 
 # fire_env = Environment(((470010.0, 485010.0), (6210010.0, 6225010.0)), 5, 0, the_world)
 # fire_env = Environment(((574995.0, 575005.0), (6199995.0, 6200005.0)), 5, 0, the_world)
 fire_prop = FirePropagation(fire_env)
-fire_prop.set_ignition_point(TimedPoint(482010, 6212010.0, 0))
+now = 0.
+# now = datetime.datetime(2019,6,1,0,0).timestamp()
+ignitionpoint = TimedPoint(482010-1000, 6212010.0, now)
+fire_prop.set_ignition_point(ignitionpoint)
 # fire_prop.set_ignition_point(TimedPoint(575000.0, 6200000.0, 0))
 
-propagation_end_time = 10000
+propagation_end_time = now + 60*60*10
 export_front = (7000, 7100)
 
 fire_prop.propagate(propagation_end_time)
@@ -54,11 +59,12 @@ fire_env.raster.write_to_file("demo_propagation_highres_dem.tif", "elevation")
 # Figure terrain + ignition contour + ignition point
 gdd = display.GeoDataDisplay.pyplot_figure(
     fire_env.raster.combine(fire_prop.ignitions().slice(["ignition"])),
-    frame=(0., 0.))
-gdd.draw_elevation_shade(with_colorbar=False, cmap=matplotlib.cm.terrain)
-gdd.draw_wind_quiver()
+    frame=(12.5, 0.))
+# gdd.draw_elevation_shade(with_colorbar=False, cmap=matplotlib.cm.terrain)
+# gdd.draw_wind_quiver()
+gdd.draw_ignition_shade(cmap=matplotlib.cm.Reds)
 gdd.draw_ignition_contour(with_labels=True, cmap=matplotlib.cm.plasma)
-gdd.draw_ignition_points(TimedPoint(482010, 6212010.0, 0))
+gdd.draw_ignition_points(ignitionpoint)
 
 gdd.figure.show()
 
