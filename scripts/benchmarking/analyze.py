8f2d302ae89050045291f6bfbb6d7d5e4c1bcca8
Rafael Bailon-Ruiz <rafael.bailon-ruiz@laas.fr>
2020-04-15 23:24:11 +0200

    Update analyze script

--- scripts/benchmarking/analyze.py
+++ scripts/benchmarking/analyze.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def main(args):
     def sample(x_space, step_func):
         """Sample a piecewise constant function at each point in x_space.
@@ -24,28 +25,35 @@ def main(args):
         return sampled
 
     # sampled times on which to plot utility history in [0.001, 10] (seconds)
-    x = np.logspace(-3, 1, num=10000)
+    x = np.linspace(0, 30, num=1000)
 
     if not os.path.isdir(args.benchmark):
         raise ValueError("Benchmark path {} is not a directory".format(args.benchmark))
     bench_name = os.path.split(args.benchmark)[1]
 
-
     # Get all benchmark run folders
     list_scenario_subfolders = [f.path for f in os.scandir(args.benchmark) if f.is_dir()]
     # TODO: Check whether folders are really benchmark runs
 
+    scenario_to_file_mapping = {}
     result_files = {}
     for subfolder in list_scenario_subfolders:
-        fold = os.path.split(subfolder)[1] ## Benchmark id == timestamp == folder name
-        result_files[fold] = [f.path for f in os.scandir(subfolder) if os.path.splitext(f)[1] == '.json']
+        fold = os.path.split(subfolder)[1]  ## Benchmark id == timestamp == folder name
+        result_files[fold] = [f.path for f in os.scandir(subfolder) if
+                              os.path.splitext(f)[1] == '.json']
         result_files[fold].sort()
+        for f in result_files[fold]:
+            scenario_id = os.path.splitext(os.path.basename(f))[0]
+            if scenario_id not in scenario_to_file_mapping:
+                scenario_to_file_mapping[scenario_id] = [f]
+            else:
+                scenario_to_file_mapping[scenario_id].append(f)
 
     # array that will be used to store one dictionnary per (benchmark-instance, vns-configuration) instance
     all_lines = []
 
-    for run_id, run_json_files in result_files.items():
-        print("Benchmark : " + str(run_id))
+    for run_id, run_json_files in scenario_to_file_mapping.items():
+        print("Benchmark instance : " + str(run_id))
         for f in run_json_files:
             print("Processing: " + str(f))
             j = {}
@@ -92,15 +100,21 @@ def main(args):
             df_of_conf = df[df["configuration_name"] == conf]
 
             # extract all utility histories and normalize them (a normalized utility of 1 means the best utility for the given instance)
-            normalized_utility_histories = [hist / u_star for (_, (hist, u_star))
+            normalized_utility_histories = [u_star / hist for (_, (hist, u_star))
                                             in
-                                            df_of_conf[['utility_history', 'utility_star']].iterrows()]
+                                            df_of_conf[
+                                                ['utility_history', 'utility_star']].iterrows()]
             # mean of all utility histories for the current configuration
             mean_utility_history = np.mean(normalized_utility_histories, axis=0)
 
-            plt.loglog(x, mean_utility_history, label=conf)
+            plt.plot(x, mean_utility_history, label=conf)
 
-        plt.legend(loc="upper right")
+        plt.xlabel("Planning time [s]")
+        plt.ylabel("Score")
+        plt.ylim((0.8, 1.))
+        plt.xlim((0., 30.))
+
+        plt.legend(loc="upper left")
         plt.show()
 
         # remove the utility history column because it is very large (each cell is an array)
@@ -108,7 +122,7 @@ def main(args):
         del df["utility_history"]
 
         # df["utility_star"] = df.apply(lambda row: best_util(row['instance']), axis=1)
-        df["utility_norm"] = (df["utility"] + 0.001) / (df["utility_star"] + 0.001)
+        df["utility_norm"] = (df["utility_star"] + 0.001) / (df["utility"] + 0.001)
 
         # superseeded by "utility_norm"
         del df["utility"]
@@ -129,12 +143,12 @@ if __name__ == "__main__":
                         help="Wait for user input before start. Useful to hold the execution while attaching to a debugger")
 
     parser.add_argument('benchmark',
-                               help="Path to a benchmark scenario")
+                        help="Path to a benchmark scenario")
     parser.add_argument('output', help="Analysis output folder path", )
     parser.add_argument("--instances", nargs='+',
-                               help="Instances considered for analysis",
-                               type=int,
-                               default=[])
+                        help="Instances considered for analysis",
+                        type=int,
+                        default=[])
     args = parser.parse_args()
 
     if args.wait:
